9e8dfe1d00495d531e733f26fd67a20b9f15a788
Sylsee <sylvio.poliart@gmail.com>
2021-03-20 18:17:39 +0100

    start to do tests with the minilibX

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: spoliart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/20 14:53:04 by spoliart          #+#    #+#             */
-/*   Updated: 2021/03/20 15:18:03 by spoliart         ###   ########.fr       */
+/*   Updated: 2021/03/20 18:17:32 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,14 @@ int main()
 	int y = 249;
 	int x = 249;
 	void *ptr = mlx_init();
-	void *win = mlx_new_window(ptr, 600, 600, "This is a test !");
+	void *win = mlx_new_window(ptr, 1920, 1080, "This is a test !");
 	while (++y <= 350)
 	{
 		x = 249;
 		while (++x <= 350)
-			mlx_pixel_put(ptr, win, x, y, 0);
+			mlx_pixel_put(ptr, win, x, y, 0x00FF0000);
 	}
+	while (++y < 600)
+		mlx_pixel_put(ptr, win, x, y, 0x00FF0000);
 	mlx_loop(ptr);
 }
